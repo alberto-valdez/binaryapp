711566044950b11302c561343e4a7ec21f85c45e
Alberto Valdez <albertovaldez03@icloud.com>
2021-03-29 15:12:35 -0600

    Primer commit proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [decimal, setDecimal] = useState(0);
+  const [resultado, setResultado] = useState('');
+
+  
+
+  async function convertir(d){
+    
+    let dec = d;
+    let residuo;
+    let resultado = '';
+    let primerDigito = dec%2;
+    resultado = primerDigito.toString();
+    while(dec>=2){
+     dec =  dec/2;
+     residuo = dec%2;
+     resultado = resultado + Math.floor(residuo);
+    }
+   let r = resultado.split('').reverse().join('');
+
+    //resumido:  
+    // let decimal = parseInt(d);
+    // let r = decimal.toString(2)
+   setResultado(r);
+   return;
+   
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Convierte un número decimal a binario</h3>
+       <input type='number' onChange={e=>{setDecimal(e.target.value)}} placeholder='0'/>
+       <button onClick={()=>{convertir(decimal)}}>Convertir</button>
+     
+       <input type='text' defaultValue={resultado} onChange={e=>{setDecimal(e.target.value)}} placeholder='Representación binaria'/>
+  
+     
       </header>
     </div>
   );
